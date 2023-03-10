cc5049c07285b2c6d65c1a4590dc3cfa68a6ad90
saulParzy19 <120722248+saulParzy19@users.noreply.github.com>
2023-03-10 13:03:07 +0100

    "Modifica"

--- Saluto_Parisi_Gruppo2.py
+++ Saluto_Parisi_Gruppo2.py
@@ -1,5 +1,9 @@
+import Saluto_Bocchi
+import Saluto_Cavallari_Gruppo2
+import Saluto_Rossi
+import Saluto_Mazzolena
 def saluto_Parisi_Gruppo():
-    saluto = "Saluti da Mattia Parisi del gruppo"
+    saluto = "Saluti da Mattia Parisi del gruppo 2 "
     print(saluto)
 
 saluto_Parisi_Gruppo()
